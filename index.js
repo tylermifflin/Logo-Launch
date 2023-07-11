7838bf5534df663229e4b799fdbb006b7d2593cc
Tyler Mifflin <you@example.com>
2023-07-11 17:22:12 -0600

    set up the inquirer prompt to use the questions in the question array

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ const { Circle, Square, Triangle } = require('./lib/shapes');
 const questions = ["What text would you like to add? (enter 1-3 characters)", "What color do you want your text to be? (use keyword or hexadecimal number)",
  "Do you want to use a circle, square, or triangle?", "What color do you want your shape to be? (use keyword or hexadecimal number)"];
 
+ inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'shape',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: questions[3],
+        },
+    ])
 
