389282138ef751a68cf10596ce445689051fed69
Tyler Mifflin <you@example.com>
2023-07-11 17:11:19 -0600

    set up the questions for inquirer and put them into an array

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Square, Triangle } = require('./lib/shapes');
 
-const questions = [""
+const questions = ["What text would you like to add? (enter 1-3 characters)", "What color do you want your text to be? (use keyword or hexadecimal number)",
+ "Do you want to use a circle, square, or triangle?", "What color do you want your shape to be? (use keyword or hexadecimal number)"];
 
 
