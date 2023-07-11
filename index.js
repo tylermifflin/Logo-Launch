150174cbc4b17a3d7a30a6be7efbc9f568123429
Tyler Mifflin <you@example.com>
2023-07-11 17:28:58 -0600

    added validate for text length to make sure it doesn't go over 3 characters

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const { Circle, Square, Triangle } = require('./lib/shapes');
 const questions = ["What text would you like to add? (enter 1-3 characters)", "What color do you want your text to be? (use keyword or hexadecimal number)",
  "Do you want to use a circle, square, or triangle?", "What color do you want your shape to be? (use keyword or hexadecimal number)"];
 
+ const startprompt = () =>
  inquirer
     .prompt([
         {
             type: 'input',
             name: 'text',
             message: questions[0],
+            validate: (text) => {
+                if (text.length > 3) {
+                    return 'Please enter 1-3 characters';
+                }
+        },
         },
         {
             type: 'input',
@@ -28,4 +34,7 @@ const questions = ["What text would you like to add? (enter 1-3 characters)", "W
             message: questions[3],
         },
     ])
+    
+
+
 
