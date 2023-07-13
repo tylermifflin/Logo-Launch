fc93d774e4cf7716f173d41b4102319f8aa8f31e
Tyler Mifflin <you@example.com>
2023-07-12 21:06:49 -0600

    setting up the createSVG function to use the responses from inquirer and if statement for circle to render if chosen

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const { Circle, Square, Triangle } = require('./lib/shapes');
 const questions = ["What text would you like to add? (enter 1-3 characters)", "What color do you want your text to be? (use keyword or hexadecimal number)",
  "Do you want to use a circle, square, or triangle?", "What color do you want your shape to be? (use keyword or hexadecimal number)"];
 
+ // function to write the svg file using the shapes library
+const createSVG = (response) => {
+    const text = response.text;
+    const textColor = response.textColor;
+    const shape = response.shape;
+    const shapeColor = response.shapeColor;
+    if (shape === "circle") {
+        const circle = new Circle();
+        circle.setColor(shapeColor);
+        const svg = circle.render();
+        fs.writeFile('image.svg', svg, (err) =>
+        err ? console.log(err) : console.log('Successfully created image.svg!')
+        );
+    } else if (shape === "square") {
+        const square = new Square();
+   
+  
+
+
+
  const startprompt = () =>
  inquirer
     .prompt([
